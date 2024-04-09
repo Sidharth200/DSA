023a677ee5e844e8a37d23b75809320cae87eade
Sidharth Lohchab <sidhugoli2003@gmail.com>
2024-04-09 13:28:43 +0530

    DSA

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,20 @@
+//4321
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            int k = n - j + 1;
+            cout<<k;
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+}
