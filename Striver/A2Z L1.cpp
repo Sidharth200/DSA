809b798f080b9743f4197c8677ec6a9388b9b3b6
Sidharth Lohchab <sidhugoli2003@gmail.com>
2024-09-14 08:52:31 +0530

    dsa

--- Striver/A2Z L1.cpp
+++ Striver/A2Z L1.cpp
@@ -4,34 +4,10 @@ using namespace std;
 
 int main() {
     
-    int day;
-    cout<<" enter day in integer: ";
-    cin>>day;
-    switch (day){
-        case 1:
-            cout<<"sunday";
-            break;
-        case 2:
-            cout<<"monday";
-            break;
-        case 3:
-            cout<<"tuesday";
-            break;
-        case 4:
-            cout<<"wednesday";
-            break;
-        case 5:
-            cout<<"thursday";
-            break;
-        case 6:
-            cout<<"friday";
-            break;
-        case 7:
-            cout<<"saturday";
-            break;
-        default:
-            cout<<"Invalid day!";
-    }
+    
+    for(int i = 1; i<=500; i++){
+        cout<<"Sidharth"<<endl;
+    };
     
     return 0;
 
